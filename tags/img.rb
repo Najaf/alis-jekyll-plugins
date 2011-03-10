78de44639ed1d4fa959c259ef60bc2bee035bc29
Najaf Ali <ali.najaf@gmail.com>
2011-03-10 02:29:30 -0500

    configified cdn host prepend in image tag

--- tags/img.rb
+++ tags/img.rb
@@ -1,5 +1,25 @@
 require 'yaml'
 module Jekyll
+    # Nice easy image tags with cdn caching spiciness
+    #     Images are expected in /images/:slug/:file.[png|jpg]
+    #     where :file is passed into the tag as text
+    #
+    # Usage:
+    #     In 2010-11-26-ochanomizu.markdown:
+    #
+    #         {%img kanda-myoujin%}
+    #
+    #     Will generate:
+    #
+    #         <img src='/images/ochanomizu/kanda-myoujin.[png|jpg]' alt='Kanda Myoujin' title='Kanda Myoujin' />
+    #
+    #     Prefers pngs to jpgs, and will error if it doesn't find at least one in the expected directory
+    #
+    #     With config.cdn_images == true and config.cdn_host = 'http://cdn.host.com' and ENV['JEKYLL_ENV'] = 'production'
+    #     (best to set that in rakefile when you're actually deploying rather than just developing/writing)
+    #
+    #         <img src='http://cdn.host.com/images/ochanomizu/kanda-myoujin.[png|jpg]' alt='Kanda Myoujin' title='Kanda Myoujin' />
+    #
     class ImageTag < Liquid::Tag
         def initialize(name, text, tokens)
             super
@@ -19,6 +39,12 @@ module Jekyll
                 img_path = img_path_base + '.jpg'
             else
                 raise "No image: #{base_dir + img_path_base}.[png|jpg] found"
+            end
+
+            # prepend cdn host if config/env values set up for it
+            config = context.registers[:site].config
+            if config['cdn_images'] and config['cdn_host'] != '' and ENV['JEKYLL_ENV'] == 'production'
+                img_path = config['cdn_host'] + '/' + img_path
             end
 
             #determine alt/title text
